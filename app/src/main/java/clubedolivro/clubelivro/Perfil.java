f546867452d5ab671ebf15c85ad8227d25da891c
Bruno Fillmann <brunofillmann@hotmail.com>
2017-10-07 22:25:04 -0300

    Inicial

---
+++ app/src/main/java/clubedolivro/clubelivro/Perfil.java
@@ -0,0 +1,13 @@
+package clubedolivro.clubelivro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
